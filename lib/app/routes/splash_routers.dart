8fdc2dc63f3a794861ed9ce6e54d3826594a59f7
filipe haniel <you@example.com>
2022-10-24 06:03:31 -0300

    first commit | cnfigurações do tema

---
+++ lib/app/routes/splash_routers.dart
@@ -0,0 +1,14 @@
+import 'package:vakinha_burger_mobile/app/modules/splash/splash_page.dart';
+
+import 'package:get/get.dart';
+
+class SplashRouters {
+  SplashRouters._();
+
+  static final routers = <GetPage>[
+    GetPage(
+      name: '/',
+      page: () => const SplashPage(),
+    ),
+  ];
+}
